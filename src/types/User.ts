095bb3078435c9791176f5c21d7c9fcecfecb58b
Chakradhar-Malage <chessp526@gmail.com>
2026-09-16 16:28:57 +0530

    Changed user data type based on api response and roles

--- src/types/User.ts
+++ src/types/User.ts
@@ -2,6 +2,9 @@ export interface User {
   id: number;
   username: string;
   email: string;
-  role: string;
-  mobileNumberVerified: boolean;
+  roles: string[];                    // ← changed to array
+  mobileNumberVerified?: boolean;
+  createdAt?: string | null;
+  totalAuctionsCreated?: number;
+  totalBidsPlaced?: number;
 }
